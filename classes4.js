6603eaacb4c0c85072c563b28240415347422e8e
Elio <elio.sanfratello@gmail.com>
2023-10-25 12:23:02 +0200

    Classes 4

---
+++ classes4.js
@@ -0,0 +1,38 @@
+class Person {
+    _firstName;
+    _lastName;
+    _age;
+    constructor(firstName, lastName, age) {
+        this._firstName = firstName;
+        this._lastName = lastName;
+        this._age = age;
+    }
+    set age(newVal) {
+        this._age = newVal
+    }
+    get age() {
+        return this._age
+    }
+    set firstName(newVal) {
+        this._firstName = newVal;
+    }
+    get firstName() {
+        return this._firstName
+    }
+    set lastName(newVal) {
+        this._lastName = newVal;
+    }
+    get lastName() {
+        return this._lastName
+    }
+    get fullName() {
+        return this._firstName + " " + this._lastName + " " + this._age
+    }
+}
+
+const person = new Person('Mario', 'Rossi', 25);
+console.log(person.fullName);
+
+person.firstName = 'Maria';
+person.lastName = 'Verdi';
+console.log(person.fullName);
